e1ffd7f43fbf8c12af42b5716005c90eec948d0e
kret-tenebris <kret.aleiva@gmail.com>
2026-05-02 13:07:25 +0300

    v_3

--- src/resource_core/include/invertedIndex.hpp
+++ src/resource_core/include/invertedIndex.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "document.hpp"
+#include <map>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -10,13 +11,12 @@ class InvertedIndex
 {
   private:
     // (word: [id_doc: count])
-    size_t doc_counter = 0;
-    std::unordered_map<std::string, std::list<std::pair<std::uint64_t, std::uint64_t>>> dictionary;
+    std::unordered_map<std::string, std::map<size_t, size_t>> dictionary;
 
   public:
-    void add(Document&& document);
-    void remove(size_t id) const;
-    std::vector<size_t> search_by_word(const std::string& word) const;
-    size_t count(std::string& word) const;
+    void add(const Document& document);
+    void remove(size_t id);
+    std::vector<size_t> searchByWord(const std::string& word) const;
+    std::map<size_t, size_t> count(const std::string& word) const;
 };
 } // namespace lab5::space
--- src/resource_core/src/document.cpp
+++ src/resource_core/src/document.cpp
@@ -6,4 +6,17 @@ Document::Document(const size_t id, const std::string& name, const std::string& 
     : id(id), name(name), content(content)
 {
 }
+
+const std::string& Document::getName() const
+{
+    return name;
+}
+const std::string& Document::getContent() const
+{
+    return content;
+}
+const size_t Document::getId() const
+{
+    return id;
+}
 } // namespace lab5::space
